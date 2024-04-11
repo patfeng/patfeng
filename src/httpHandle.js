a09f4699d3545faf5fd2b0314b93f88880be1371
Pat Feng <patrickrfeng@gmail.com>
2024-04-10 22:38:00 -0400

    mom
    tempdel
    
    temp

--- src/httpHandle.js
+++ src/httpHandle.js
@@ -1,49 +1,25 @@
 import axios from 'axios';
+import OpenAI from 'openai';
 
-export async function  getResponse(prompt,patCurrText){
+const openai = new OpenAI({
+    apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
+});
 
-    const stream = await generateStream(prompt)
-        for await (const chunk of stream) {
-        const jsonObj = JSON.parse(chunk);
+export async function getResponse(prompt,patCurrText){
+
+    const stream = await openai.chat.completions.create({
+      model: 'gpt-4',
+      messages: [{ role: 'user', content: 'Say this is a test' }],
+      stream: true,
+    });
+    for await (const chunk of stream) {
+      const jsonObj = JSON.parse(chunk);
         patCurrText.text=patCurrText.text + jsonObj.response; 
     }
+
+
 }
 
-export async function* getIterableStream(body) {
-    const reader = body.getReader();
-    const decoder = new TextDecoder();
+function buildQuery(prompt){
   
-    while (true) {
-      const { value, done } = await reader.read();
-      if (done) {
-        break;
-      }
-  
-      const decodedChunk = decoder.decode(value, { stream: true });
-      yield decodedChunk;
-    }
-  }
-  
-  export const generateStream = async (prompt) => {
-    const response = await fetch(
-        'http://localhost:11434/api/generate',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-            "model": 'pat0.0',
-            "prompt": prompt
-        }),
-        responseType: 'stream'
-      }
-    );
-  
-    if (response.status !== 200) {
-      throw new Error(response.status.toString());
-    }
-  
-    if (!response.body) {
-      throw new Error('Response body does not exist');
-    }
-  
-    return getIterableStream(response.body);
-  };
+}
